6287f45fcc8d60c643fb0cc4e6049a14af0bfec4
Soham Barate <sohambarate16@gmail.com>
2026-01-22 21:28:05 +0530

    chore: Configure deployment to gh-pages branch

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -43,8 +43,8 @@ export const OperatorBackground = memo(function OperatorBackground() {
       alpha: number;
       
       constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = Math.random() * canvas!.width;
+        this.y = Math.random() * canvas!.height;
         this.vx = (Math.random() - 0.5) * 0.5;
         this.vy = (Math.random() - 0.5) * 0.5;
         this.size = Math.random() * 2 + 1;
@@ -55,8 +55,8 @@ export const OperatorBackground = memo(function OperatorBackground() {
         this.x += this.vx;
         this.y += this.vy;
         
-        if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
-        if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
+        if (this.x < 0 || this.x > canvas!.width) this.vx *= -1;
+        if (this.y < 0 || this.y > canvas!.height) this.vy *= -1;
       }
       
       draw() {
