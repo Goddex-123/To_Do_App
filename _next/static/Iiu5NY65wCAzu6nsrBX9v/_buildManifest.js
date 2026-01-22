57fac5e9f6239a21d270f5e86d064726bfae22e9
Soham Barate <sohambarate16@gmail.com>
2026-01-22 21:26:21 +0530

    Updates

---
+++ _next/static/Iiu5NY65wCAzu6nsrBX9v/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/To_Do_App//_next/:path+",
+        "destination": "/To_Do_App/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
